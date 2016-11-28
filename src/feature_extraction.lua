4d4b47303666b6e11c6cc30a9b486c1e4bba42d9
laurent <laurent.cetinsoy@gmail.com>
2016-11-28 17:00:14 +0100

    remove custom function for copy

--- src/feature_extraction.lua
+++ src/feature_extraction.lua
@@ -1,45 +1,47 @@
 require 'torch'
 require 'image'
 require 'nn'
-net = torch.load('../input/VGG_FACE.t7')
-net:evaluate()
+csvfile = require "src/simplecsv"
 
-
-input = csvfile.read('../output/cleaned.csv')
-
-function table.shallow_copy(t)
-
-  local t2 = {}
-  for i=1, 4096 do
-    t2[#t2 + 1] = t[i]
-  end
-  return t2
-end
-
+input = csvfile.read('output/cleaned.csv')
 
 output = nil
 output = {}
-path="/home/nawel/Downloads/wiki/wiki_crop/"
-print(output, #output)
-for i=2,#input do
-    im = nil
-    im_scaled = nil
-    im_bgr = nil
-    features = nil
 
-    im = image.load(path .. input[i][5],3,'float')
+path = "input/wiki_crop/"
 
-    im_scaled = image.scale(im,"224x224")
-    im_scaled = im_scaled*255
+
+net = torch.load('input/VGG_FACE.t7')
+net:evaluate()
+
+net.modules[35] = nil
+net.modules[36] = nil
+net.modules[37] = nil
+net.modules[38] = nil
+net.modules[39] = nil
+net.modules[40] = nil
+collectgarbage()
+net:evaluate()
+
+
+--10001-20000
+for i = 2, 3 do
+
+    im = image.load(path .. input[i][2], 3, 'float')
+
+    im_scaled = image.scale(im, "224x224")
+    im_scaled = im_scaled * 255
 
     mean = {129.1863,104.7624,93.5940}
-    im_bgr = im_scaled:index(1,torch.LongTensor{3,2,1})
+    im_bgr = im_scaled:index(1, torch.LongTensor{3,2,1})
+
     for h = 1, 3 do
         im_bgr[h]:add(-mean[h])
     end
     features=net(im_bgr)
 
-    --table.insert(output, features )--output[i-1]=net(im_bgr)
-    output[#output + 1] = table.shallow_copy(features)
-    --print(#output, output, features[1], features[545])
+    output[#output + 1] = torch.totable(features)
+
 end
+
+csvfile.write('output/output.csv', output)

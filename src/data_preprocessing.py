a12aa9eab8a916e26333ffd85ac9783c331fe558
laurent <laurent.cetinsoy@gmail.com>
2016-12-14 18:36:01 +0100

    script for building raw data

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -4,7 +4,7 @@ from datetime import date
 import datetime
 import re
 import numpy as np
-
+from scipy.misc import imread, imresize
 
 def ordinalToDate(ordinal):
     return date.fromordinal(ordinal) + datetime.timedelta(days=ordinal % 1) - datetime.timedelta(days=366)
@@ -29,8 +29,8 @@ wikiData = pd.DataFrame(data={
 
     'date_of_birth': dateOfBirths,
     'date_of_photo_shoot': dateOfPhotoShoot,
-
     'file_path': filePath,
+    'faceScore': faceScore
 
 })
 
@@ -47,5 +47,34 @@ wikiData = wikiData.drop('date_of_photo_shoot', 1)
 wikiData['label'] = wikiData['age'].apply(binarize)
 cleanData = wikiData.replace([np.inf, -np.inf], np.nan).dropna()
 
-cleanData.to_csv('output/cleaned.csv')
+# cleanData.to_csv('output/cleaned.csv')
 # wikiData.to_csv('output/example.csv')
+
+# print(cleanData.shape)
+numberOfRows = cleanData.shape[0]
+#
+# print(cleanData['file_path'][:10])
+# print(cleanData['label'][:10])
+nFeature = 224 * 224 * 3
+
+
+print("Build raw data set")
+with open(r'test.txt', 'w') as f:
+
+    for i in cleanData.index[:1000]:
+
+        img = imread("input/wiki_crop/" + cleanData['file_path'][i])
+
+        label = cleanData['label'][i]
+        img = imresize(img, (224, 224))
+
+        if (len(img.shape) == 2):
+            img = img.reshape((224 * 224))
+            img = np.hstack((img,img,img, label))
+        else:
+            img = img.reshape(nFeature)
+            img = np.hstack((img, label))
+
+        f.write(" ".join(np.char.mod('%d', img)) + "\n")
+
+f.close()
